36c52961c4708f26500cb54c3632987812087898
Aleksei Krasnov <alexey-krasnov@rambler.ru>
2022-01-31 06:53:23 +0300

    Band potential calculator_works

--- band_potential_calc.py
+++ band_potential_calc.py
@@ -10,9 +10,15 @@ df = pd.read_csv('table_energies_of_elements.csv', sep=',', index_col='Element')
 # df['Electronegativity'] = (df['Electron affinity (eV)'] + df['1st Ionization Potential (eV)']) / 2
 
 def get_formula():
+    """Get formula of semiconductor from user input"""
     semiconductor = input("Please, enter a semiconductor formula: ")
+    return semiconductor
+
+
+def parse_chem_formula(semiconductor):
+    """Parse semiconductor formula and return as dictionary {'element': index, etc}"""
     formula_as_dict = chemparse.parse_formula(semiconductor)
-    return semiconductor, formula_as_dict
+    return formula_as_dict
 
 
 def get_eg():
@@ -38,7 +44,8 @@ def get_band_potentials():
     return e_cb, e_vb
 
 
-semiconductor, formula_as_dict = get_formula()
+semiconductor = get_formula()
+formula_as_dict = parse_chem_formula(semiconductor)
 band_gap = get_eg()
 semicond_electronegativity = get_electronegativity()
 e_cb, e_vb = get_band_potentials()
@@ -46,11 +53,11 @@ e_cb, e_vb = get_band_potentials()
 print(f"{semiconductor} has band gap {band_gap} ev, Ecb is {round(e_cb, 2)} eV, and Evb is {round(e_vb, 2)} eV")
 
 # Make DataFrame for output information of the processed semiconductors
-col_names = ['Band gap, eV', 'Ecb, eV', 'Evb, eV']
-df_out = pd.DataFrame(columns=col_names)
-df_out.loc[semiconductor] = [band_gap, e_cb, e_vb]
-print(df_out)
-df_out.to_csv('out_data.csv')
+# col_names = ['Band gap, eV', 'Ecb, eV', 'Evb, eV']
+# df_out = pd.DataFrame(columns=col_names)
+# df_out.loc[semiconductor] = [band_gap, e_cb, e_vb]
+# print(df_out)
+# df_out.to_csv('out_data.csv')
 
 # To check semiconductors from list, for QA tests
 # semiconds = {'BaTaO2N' : 1.49, 'BaTa0.5Al0.5O2N': 1.61, 'BaTa0.5Mg0.5O2N' : 2.01, 'BaTa0.5Al0.375Mg0.125O2N': 1.36}
