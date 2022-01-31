e15d79427f0a3de24b0bfaf13b42d6e60b63423d
Aleksei Krasnov <alexey-krasnov@rambler.ru>
2022-01-31 07:08:33 +0300

    Band potential calculator_works

--- band_potential_calc.py
+++ band_potential_calc.py
@@ -44,20 +44,39 @@ def get_band_potentials(eg):
     return e_cb, e_vb
 
 
-semiconductor = get_formula()
-formula_as_dict = parse_chem_formula(semiconductor)
-band_gap = get_eg()
-semicond_electronegativity = get_electronegativity()
-e_cb, e_vb = get_band_potentials(band_gap)
-
-print(f"{semiconductor} has band gap {band_gap} ev, Ecb is {round(e_cb, 2)} eV, and Evb is {round(e_vb, 2)} eV")
+semicond_dict = {'BaTaO2N': 1.49, 'BaTa0.5Al0.5O2N': 1.61, 'BaTa0.5Mg0.5O2N': 2.01, 'BaTa0.5Al0.375Mg0.125O2N': 1.36}
 
 # Make DataFrame for output information of the processed semiconductors
 col_names = ['Band gap, eV', 'Ecb, eV', 'Evb, eV']
 df_out = pd.DataFrame(columns=col_names)
-df_out.loc[semiconductor] = [band_gap, e_cb, e_vb]
+
+if semicond_dict:
+    for semicond, band in semicond_dict.items():
+        formula_as_dict = parse_chem_formula(semicond)
+        semicond_electronegativity = get_electronegativity()
+        e_cb, e_vb = get_band_potentials(band)
+        df_out.loc[semicond] = [band, e_cb, e_vb]
+else:
+    semiconductor = get_formula()
+    formula_as_dict = parse_chem_formula(semiconductor)
+    band_gap = get_eg()
+    semicond_electronegativity = get_electronegativity()
+    e_cb, e_vb = get_band_potentials(band_gap)
+    df_out.loc[semiconductor] = [band_gap, e_cb, e_vb]
 print(df_out)
 df_out.to_csv('out_data.csv')
+
+
+
+
+# print(f"{semiconductor} has band gap {band_gap} ev, Ecb is {round(e_cb, 2)} eV, and Evb is {round(e_vb, 2)} eV")
+
+# Make DataFrame for output information of the processed semiconductors
+# col_names = ['Band gap, eV', 'Ecb, eV', 'Evb, eV']
+# df_out = pd.DataFrame(columns=col_names)
+# df_out.loc[semiconductor] = [band_gap, e_cb, e_vb]
+# print(df_out)
+# df_out.to_csv('out_data.csv')
 
 # To check semiconductors from list, for QA tests
 # semiconds = {'BaTaO2N': 1.49, 'BaTa0.5Al0.5O2N': 1.61, 'BaTa0.5Mg0.5O2N': 2.01, 'BaTa0.5Al0.375Mg0.125O2N': 1.36}
